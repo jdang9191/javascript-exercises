6b12eae9724be2eb93ccd2a1a5a566d4e42592b8
jdang9191 <jdang9191@gmail.com>
2023-09-21 15:36:14 -0700

    repeat String

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(hey, num) {
+    let string = ""
+    for(var i = 0; i < num; i++){
+        string += hey;
+    }
+    return string;
 };
 
 // Do not edit below this line

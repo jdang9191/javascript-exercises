b235d106385dd5b70c0934167e9d8a63aa61b9d7
jdang9191 <jdang9191@gmail.com>
2023-09-21 17:24:50 -0700

    reversed string function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let r = word.length - 1;
+    let returnString = "";
+    while(r >= 0){
+        returnString += word[r];
+        r--;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+
+
+/*
+hello
+
+separate int gets p
+h goes to p's place
+
+*/

8c21b37c8ae7e9d6c5ca91c1b84a3995e0d55ca1
jdang9191 <jdang9191@gmail.com>
2023-10-05 15:07:46 -0700

    new:

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    let celcius = (temp-32) * (5/9)
+    celcius = Math.round(celcius * 10)/10
+
+    return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = temp *(9/5) + 32
+  fahrenheit = Math.round(fahrenheit * 10)/10
+
+  return fahrenheit;
 };
 
 // Do not edit below this line
